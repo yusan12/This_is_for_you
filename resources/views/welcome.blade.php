e865581bd8129db0f702a96326ac05494752db9c
yusan12 <haraheri7212@yahoo.co.jp>
2020-02-26 17:06:38 +0700

    コミットメッセージ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,10 +37,10 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}"></a>
+                        <a href="{{ route('login') }}">login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}"></a>
+                            <a href="{{ route('register') }}">register</a>
                         @endif
                     @endauth
                 </div>
