7a20d7f7e8dda883723bcfbd3fdfa19a91eab49a
yusan12 <haraheri7212@yahoo.co.jp>
2020-02-26 16:53:59 +0700

    コミットメッセージ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,10 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('public/css/welcome.css') }}">
+        
+        <link href="public/css/welcome.css" rel="stylesheet">
+
+
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
