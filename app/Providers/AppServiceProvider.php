52f1ef2f40ca96bfe2d317673e6f34c546fda03b
YuGyao <[your>
2020-05-26 15:34:54 +0900

    SSL接続テスト３

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        
+        // 本番環境(Heroku)でhttpsを強制する
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
-        
     }
 }
